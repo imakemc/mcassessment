0e37646430dc6fd5ab9cb4688aea22abe11b7f82
Kyoya <excluzard.360@gamail.com>
2015-05-07 00:07:17 +0700

    up2u

---
+++ WebContent/resources/js/mcassessment/mcassessment.js
@@ -0,0 +1,23 @@
+
+//function doAjaxCenter(url,param,type,method){
+////	path = contextPath + '/mainfeature/contact/search';
+//	path = url;
+//	var vData = param;
+//	$.ajax({
+//		url : path,
+//		data : vData,
+//		cache : false,
+//		method : type,
+//		dataType : "json",
+//		async : true,
+//		success : function(output) {
+//			if (output != null) { 
+//				method()
+//			} else {
+//				checkSession(output);
+//				alert("No data found.");
+//			}
+//		}
+//	});
+//	
+//}
