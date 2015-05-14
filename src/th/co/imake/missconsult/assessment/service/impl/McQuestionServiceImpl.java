dd2cc6e2dd5b1b51b83a7e6f59fd0a37a25d91ef
xOffice <user@user-PC>
2015-05-14 19:25:25 +0700

    xOffice_14/05/2015

--- src/th/co/imake/missconsult/assessment/service/impl/McQuestionServiceImpl.java
+++ src/th/co/imake/missconsult/assessment/service/impl/McQuestionServiceImpl.java
@@ -1,12 +1,31 @@
 package th.co.imake.missconsult.assessment.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import th.co.imake.missconsult.assessment.domain.McQuestion;
+import th.co.imake.missconsult.assessment.model.McQuestionM;
 import th.co.imake.missconsult.assessment.repository.McQuestionRepository;
 import th.co.imake.missconsult.assessment.service.McQuestionService;
 @Service("mcQuestionService")
 public class McQuestionServiceImpl implements McQuestionService{
 	@Autowired
 	private McQuestionRepository mcQuestionRepository;
+
+	@Override
+	public List<McQuestionM> selectAllByMeId(Integer meid) {
+		return mcQuestionRepository.selectAllByMeId(meid);
+	}
+
+	@Override
+	public Integer insertAll(McQuestion mcQuestion) {
+		return mcQuestionRepository.insertAll(mcQuestion);
+	}
+	
+	@Override
+	public Integer deleteByMqId(Integer mqid){
+		return mcQuestionRepository.deleteByMqId(mqid);
+	}
 }
